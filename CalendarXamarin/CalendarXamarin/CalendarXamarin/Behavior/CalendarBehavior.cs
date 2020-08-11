ec6a85a10588655f851f4f37800d449838c67071
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-08-11 14:32:26 +0530

    XAMARIN-35022 - Sample and screen shot added.

---
+++ CalendarXamarin/CalendarXamarin/CalendarXamarin/Behavior/CalendarBehavior.cs
@@ -0,0 +1,43 @@
+﻿using Syncfusion.SfCalendar.XForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CalendarXamarin.Behavior
+{
+    public class CalendarBehavior : Behavior<ContentPage>
+    {
+        private SfCalendar calendar;
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            base.OnAttachedTo(bindable);
+            this.calendar = bindable.FindByName<SfCalendar>("calendar");
+            this.calendar.OnMonthCellLoaded += Calendar_OnMonthCellLoaded;
+        }
+
+        private void Calendar_OnMonthCellLoaded(object sender, MonthCellLoadedEventArgs args)
+        {
+            // As default setting Month cell Background color as Green 
+           args.BackgroundColor = Color.Green;
+           var appointments = calendar.DataSource as CalendarEventCollection;
+
+            for (int i = 0; i < appointments.Count; i++)
+            {
+                var appointment = appointments[i];
+                if (args.Date.Date == appointment.StartTime.Date)
+                {
+                    // Setting Background color when the appointment available on the specific day 
+                    args.BackgroundColor = Color.Red;
+                }
+            }
+        }
+
+        protected override void OnDetachingFrom(ContentPage bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            this.calendar.OnMonthCellLoaded -= Calendar_OnMonthCellLoaded;
+        }
+    }
+}
